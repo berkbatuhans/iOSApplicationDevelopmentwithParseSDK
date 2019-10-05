2d2e5621a09c3944104eece31ea779d366d3e03b
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2019-10-05 20:37:13 +0300

    updated.

--- ParseServeriOSDemo/Extensions/UIView+Extension.swift
+++ ParseServeriOSDemo/Extensions/UIView+Extension.swift
@@ -21,30 +21,57 @@ extension UIView {
     }
 }
 
-
+@IBDesignable
 class GradientView: UIView {
-    lazy fileprivate var gradientLayer: CAGradientLayer = {
-        let layer = CAGradientLayer()
-        layer.colors = [UIColor.clear.cgColor, UIColor(white: 0.0, alpha: 0.75).cgColor]
-        layer.locations = [NSNumber(value: 0.0 as Float),NSNumber(value: 1.0 as Float)]
-        //.layerMaxXMinYCorner -> Sağ Üst
-        layer.maskedCorners = [.layerMaxXMaxYCorner,.layerMinXMaxYCorner]
-        
-        layer.cornerRadius = 15
-        return layer
-    }()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        backgroundColor = UIColor.clear
-        layer.addSublayer(gradientLayer)
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
     }
     
     override func layoutSubviews() {
-        super.layoutSubviews()
-        CATransaction.begin()
-        CATransaction.setDisableActions(true)
-        gradientLayer.frame = bounds
-        CATransaction.commit()
+        let gradientLayer =  CAGradientLayer()
+        
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
     }
+    
 }
+
+
+//class GradientView: UIView {
+//    lazy fileprivate var gradientLayer: CAGradientLayer = {
+//        let layer = CAGradientLayer()
+//        layer.colors = [UIColor.clear.cgColor, UIColor(white: 0.0, alpha: 0.75).cgColor]
+//        layer.locations = [NSNumber(value: 0.0 as Float),NSNumber(value: 1.0 as Float)]
+//        //.layerMaxXMinYCorner -> Sağ Üst
+//        layer.maskedCorners = [.layerMaxXMaxYCorner,.layerMinXMaxYCorner]
+//
+//        layer.cornerRadius = 15
+//        return layer
+//    }()
+//
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        backgroundColor = UIColor.clear
+//        layer.addSublayer(gradientLayer)
+//    }
+//
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        CATransaction.begin()
+//        CATransaction.setDisableActions(true)
+//        gradientLayer.frame = bounds
+//        CATransaction.commit()
+//    }
+//}
